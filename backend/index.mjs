0e065bc0e1cf2fe103c74869f27eab105bd2c74b
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-21 12:15:43 +0200

    Add comments

--- backend/index.mjs
+++ backend/index.mjs
@@ -13,6 +13,7 @@ const user = {
 
 function decodeBasicToken(request) {
     const [ authType, b64token ] = request.headers.authorization.split(" ")
+    //Las siguientes 2 lineas sustituyen a atb (ascii to binary): Se pasa de asci a binario
     const tokenBuffer = new Buffer.from(b64token, "base64")
     const token = tokenBuffer.toString()
     return token.split(":")
@@ -21,6 +22,8 @@ function decodeBasicToken(request) {
 function authMiddleware (req, res, next) {
     try {
         const [ method, token ] = req.headers.authorization.split(" ")
+                // Verificación: Verify decodifica el token si es válido
+
         const { level } = jwt.verify(token,secret)
         res.locals.level = level // Paso el user name para uso en los controllers
         next()
@@ -29,11 +32,17 @@ function authMiddleware (req, res, next) {
     }
 }
 
+//1. Endpoint Autenticación
 app.get("/login/", (req, res)=>{
     const [ username, password ] = decodeBasicToken(req)
     if ( 
+        /* Aquí iria la comprobación de que el usuario y pass existan
+       Si hay base de datos, y el usuario y contraseña están en la base de datos,
+       aquí meteríamos una query de verificación
+       */
         username === user.username && password === user.password
     ) {
+        //Creación de token/firma con un secret
         const token = jwt.sign(
             {
                 level: user.accessLevel
@@ -49,6 +58,7 @@ app.get("/login/", (req, res)=>{
     }
 })
 
+//2. Uso del token
 app.get("/secretos/", authMiddleware, (req, res)=>{
     res.send(`El secreto de la vida, el universo y de todo: 42`)
 })
