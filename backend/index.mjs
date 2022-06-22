b55a4d13c564f9f3758c254172c79acc00bd62ba
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-22 09:37:59 +0200

    Add more comments

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,4 +1,4 @@
-import express  from "express";
+import express from "express";
 import jwt from "jsonwebtoken";
 
 const app = express();
@@ -12,19 +12,19 @@ const user = {
 }
 
 function decodeBasicToken(request) {
-    const [ authType, b64token ] = request.headers.authorization.split(" ")
+    const [authType, b64token] = request.headers.authorization.split(" ")
     //Las siguientes 2 lineas sustituyen a atb (ascii to binary): Se pasa de asci a binario
     const tokenBuffer = new Buffer.from(b64token, "base64")
     const token = tokenBuffer.toString()
     return token.split(":")
 }
 
-function authMiddleware (req, res, next) {
+function authMiddleware(req, res, next) {
     try {
-        const [ method, token ] = req.headers.authorization.split(" ")
-                // Verificación: Verify decodifica el token si es válido
+        const [method, token] = req.headers.authorization.split(" ")
+        // Verificación: Verify decodifica el token si es válido
 
-        const { level } = jwt.verify(token,secret)
+        const { level } = jwt.verify(token, secret)
         res.locals.level = level // Paso el user name para uso en los controllers
         next()
     } catch (err) {
@@ -33,16 +33,18 @@ function authMiddleware (req, res, next) {
 }
 
 //1. Endpoint Autenticación
-app.get("/login/", (req, res)=>{
-    const [ username, password ] = decodeBasicToken(req)
-    if ( 
+app.get("/login/", (req, res) => {
+    const [username, password] = decodeBasicToken(req)
+    if (
         /* Aquí iria la comprobación de que el usuario y pass existan
        Si hay base de datos, y el usuario y contraseña están en la base de datos,
        aquí meteríamos una query de verificación
        */
         username === user.username && password === user.password
     ) {
-        //Creación de token/firma con un secret
+        /*Creación de token/firma con un secret.
+        En este token podemos incluir o no lo que queramos
+        */
         const token = jwt.sign(
             {
                 level: user.accessLevel
@@ -59,8 +61,8 @@ app.get("/login/", (req, res)=>{
 })
 
 //2. Uso del token
-app.get("/secretos/", authMiddleware, (req, res)=>{
+app.get("/secretos/", authMiddleware, (req, res) => {
     res.send(`El secreto de la vida, el universo y de todo: 42`)
 })
 
-app.listen(3000, ()=>{console.log("Ready...");})
+app.listen(3000, () => { console.log("Ready..."); })
